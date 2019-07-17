f3bccec1f014a94e6ad53c23f5b3b4eb9b019168
kk <just44register@outlook.com>
2019-07-17 23:52:13 +0800

    添加搜索条件 searchString

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,13 @@ namespace MvcMovie.Models
     {
         public int Id{get;set;}
         public string Title { get; set; }
+
+        [Display(Name = "发行时间")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
